1533a21538394afabd6aa03622300a7028f07c3e
Min Shin Khant <minshinkhant7@gmail.com>
2020-02-09 17:15:36 -0500

    updating database location to heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-perpendicular-82571',
     user : 'min',
     password : '',
     database : 'smart-brain'
